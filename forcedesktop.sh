feb36e01b8540b90696cbd1a51fa87ba5a0eafff
liniuxchan <189403384+liniuxchan@users.noreply.github.com>
2024-12-02 00:28:17 +0900

    ちょっと進んだ！

--- forcedesktop.sh
+++ forcedesktop.sh
@@ -11,6 +11,7 @@ pkill vnc
 pkill xrdp
 pkill xfce
 tracker3 reset --filesystem
+tracker3 reset --hard
 gsettings set org.freedesktop.Tracker3.Miner.Files index-recursive-directories "[]"
 gsettings set org.freedesktop.Tracker3.Miner.Files ignored-directories "['/root']"
 
