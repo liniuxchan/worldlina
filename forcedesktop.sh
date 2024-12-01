2a2bdf5fac051c7069d88295a0997f0c9a6133d0
liniuxchan <189403384+liniuxchan@users.noreply.github.com>
2024-12-01 22:43:36 +0900

    ちょっと進んだ！

---
+++ forcedesktop.sh
@@ -0,0 +1,25 @@
+# xRDPとバックエンドをインストール
+
+dnf install epel-release -y
+dnf install tigervnc-server -y
+dnf install xrdp -y
+dnf install xfce4 -y
+
+# xRDPとバックエンドを直接、クリーン
+
+pkill vnc
+pkill xrdp
+pkill xfce
+
+# xRDPとバックエンドを直接、起動
+
+vncserver :1
+xrdp-sesman
+sed -i '/port=-1/a exec=startxfce4' /etc/xrdp/xrdp.ini
+sed -i 's/port=-1/port=5901/' /etc/xrdp/xrdp.ini
+xrdp
+
+
+
+
+
