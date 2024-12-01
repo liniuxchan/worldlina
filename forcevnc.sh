2f136d011eea5287a8cb0f1ad5efcce9787343ce
liniuxchan <189403384+liniuxchan@users.noreply.github.com>
2024-12-01 22:27:20 +0900

    完成！簡単vnc or rdp！

--- forcevnc.sh
+++ forcevnc.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-# パスワードを入力させる
-echo "パスワードを入力してください: "
-read -s password
-
-# 入力されたパスワードを表示（テスト用、実際には表示しない方が良い）
-echo "入力されたパスワード: $password"
-
-
 dnf install tigervnc-server -y
 pkill vnc
 vncserver :1
